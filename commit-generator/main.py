a9cb337b4329836258ae25ee31bab9db2b06026b
WMRamadan <thebigguyis@gmail.com>
2020-08-18 19:45:06 +0300

    {}

---
+++ commit-generator/main.py
@@ -0,0 +1,14 @@
+import string
+import os
+
+
+def letters_file_line(n):
+   with open("words1.txt", "w") as f:
+       alphabet = string.ascii_uppercase
+       letters = [alphabet[i:i + n] + "\n" for i in range(0, len(alphabet), n)]
+       f.writelines(letters)
+       os.system('git add .')
+       os.system('git commit -m "{}"').format(letters)
+
+
+letters_file_line(3)
